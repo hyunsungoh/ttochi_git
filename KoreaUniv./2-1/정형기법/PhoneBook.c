b0f47294374f2d871044e5d4a0d8d65d844266cd
kyiel1018 <kyiel1018@korea.ac.kr>
2022-03-28 02:12:05 +0900

    220328

---
+++ KoreaUniv./2-1/정형기법/PhoneBook.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NAME_LEN    20
+#define PHONE_LEN   20
+
+
+typedef struct TEL
+{
+    char name[NAME_LEN];
+    char phone[PHONE_LEN];
+}TEL;
+
+void FindOp()
+{
+    for(i=0;i=<100;i++)
+    {
+        if (name[i]>=10000 && name[i]<=99999)
+        {
+            
+        }
+
+        if (name[i] = -1)
+        {
+            
+        }
+    }
+}
---
+++ KoreaUniv./example/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("안녕하세요!");
+    return 0;
+}
